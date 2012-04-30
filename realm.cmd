726277475e6560b59bf095117161ecf61138b0b9
Björn Bidar <theodorstormgrade@gmail.com>
2012-04-30 22:23:22 +0200

    modified:   realm 	added execute in execute_app for mingw/windows new file:   realm.cmd 	added wrapper file for mingw/windows new file:   sh_makefile_mingw 	added sh_makefile for mingw/windows

---
+++ realm.cmd
@@ -0,0 +1,12 @@
+: realm wrapper script for realm on mingw
+
+set XDG_DATA_HOME=%CD%\user_data\share
+set XDG_CACHE_HOME=%CD%\user_data\cache
+set XDG_CONFIG_HOME=%CD%\user_data\config
+
+
+mkdir %XDG_DATA_HOME% > NUL
+mkdir %XDG_CACHE_HOME% > NUL
+mkdir %XDG_CONFIG_HOME% > NUL
+
+bash %CD%\realm %*
